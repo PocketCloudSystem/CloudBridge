a3dde760c3c67b04e76749fcc95b70f09a6a0d18
r3pt1s <59124150+r3pt1s@users.noreply.github.com>
2023-06-04 11:23:38 +0200

    make-phar

--- make-phar.php
+++ make-phar.php
@@ -1,8 +1,8 @@
 <?php
 
-if (file_exists(__DIR__ . "/PocketCloud.phar")) unlink(__DIR__ . "/PocketCloud.phar");
+if (file_exists(__DIR__ . "/CloudBridge.phar")) unlink(__DIR__ . "/CloudBridge.phar");
 
-$phar = new Phar(__DIR__ . "/PocketCloud.phar", 0, "PocketCloud.phar");
+$phar = new Phar(__DIR__ . "/CloudBridge.phar", 0, "CloudBridge.phar");
 $phar->buildFromDirectory(__DIR__ . "/", "/\.php$/");
 if (isset($phar["make-phar.php"])) unset($phar["make-phar.php"]);
 if (isset($phar["make-phar.bat"])) unset($phar["make-phar.bat"]);
