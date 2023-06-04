7adfda373a2d58eb8c188f588db34ed5fff3bef8
r3pt1s <59124150+r3pt1s@users.noreply.github.com>
2023-06-04 13:50:26 +0200

    remove make-phar

--- make-phar.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (file_exists(__DIR__ . "/CloudBridge.phar")) unlink(__DIR__ . "/CloudBridge.phar");
-
-$phar = new Phar(__DIR__ . "/CloudBridge.phar", 0, "CloudBridge.phar");
-$phar->buildFromDirectory(__DIR__ . "/", "/\.php$/");
-if (isset($phar["make-phar.php"])) unset($phar["make-phar.php"]);
-if (isset($phar["make-phar.bat"])) unset($phar["make-phar.bat"]);
-$phar->compressFiles(Phar::GZ);
